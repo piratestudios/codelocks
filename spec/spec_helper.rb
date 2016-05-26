107267937fb65837ea702f8733e24f84c3b96617
Robert May <rob@afternoonrobot.co.uk>
2016-05-26 14:23:55 +0100

    Update tests to cover new netcode fetching

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,5 +11,5 @@ VCR.configure do |config|
   config.filter_sensitive_data("<BASE_URI>") { Codelocks.base_uri }
   config.filter_sensitive_data("<API_KEY>") { Codelocks.api_key }
   config.filter_sensitive_data("<ACCESS_KEY>") { Codelocks.access_key }
-  config.filter_sensitive_data("<LOCK_ID>") { ENV['CODELOCKS_LOCK_ID'] }
+  config.filter_sensitive_data("LOCK_ID") { ENV['CODELOCKS_LOCK_ID'] }
 end
