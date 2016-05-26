e453478c5dd201c719de7306b7dfc40f79966e1a
Robert May <rob@afternoonrobot.co.uk>
2016-05-26 14:56:52 +0100

    Update documentation
    Resolves #2 and #3

--- lib/codelocks/lock.rb
+++ lib/codelocks/lock.rb
@@ -1,7 +1,7 @@
 module Codelocks
   class Lock
     class << self
-      # Fetch a list of locks
+      # Fetch a list of locks. Requires the access key env var to be set
       #
       # @return [Codelocks::NetCode::Response]
 
